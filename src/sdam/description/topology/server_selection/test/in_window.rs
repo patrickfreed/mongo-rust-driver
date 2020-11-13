204a7d5f9c8118170373bd70b572c48e38b2b67e
Patrick Freed <patrick.freed@mongodb.com>
2020-11-13 17:25:18 -0500

    update test to new format

--- src/sdam/description/topology/server_selection/test/in_window.rs
+++ src/sdam/description/topology/server_selection/test/in_window.rs
@@ -6,46 +6,73 @@ use serde::Deserialize;
 use crate::{
     options::StreamAddress,
     sdam::{description::topology::server_selection, Server},
+    selection_criteria::ReadPreference,
     test::run_spec_test,
 };
+
+use super::TestTopologyDescription;
 
 #[derive(Debug, Deserialize)]
 struct TestFile {
     description: String,
-    in_window: Vec<TestPoolDescription>,
+    topology_description: TestTopologyDescription,
+    mocked_topology_state: Vec<TestServer>,
+    iterations: u32,
+    outcome: TestOutcome,
+}
+
+#[derive(Debug, Deserialize)]
+struct TestOutcome {
+    tolerance: f64,
     expected_frequencies: HashMap<StreamAddress, f64>,
 }
 
 #[derive(Debug, Deserialize)]
-struct TestPoolDescription {
+struct TestServer {
     address: StreamAddress,
     operation_count: u32,
 }
 
-const TEST_ITERATIONS: u32 = 10000;
-
 async fn run_test(test_file: TestFile) {
+    println!("Running {}", test_file.description);
+
     let mut tallies: HashMap<StreamAddress, u32> = HashMap::new();
 
-    let servers: Vec<Arc<Server>> = test_file
-        .in_window
+    let servers: HashMap<StreamAddress, Arc<Server>> = test_file
+        .mocked_topology_state
         .into_iter()
-        .map(|desc| Arc::new(Server::new_mocked(desc.address, desc.operation_count)))
+        .map(|desc| {
+            (
+                desc.address.clone(),
+                Arc::new(Server::new_mocked(desc.address, desc.operation_count)),
+            )
+        })
         .collect();
 
-    for _ in 0..TEST_ITERATIONS {
+    let topology_description = test_file
+        .topology_description
+        .into_topology_description(None)
+        .unwrap();
+
+    let read_pref = ReadPreference::Nearest {
+        options: Default::default(),
+    }
+    .into();
+
+    for _ in 0..test_file.iterations {
         let selection =
-            server_selection::select_server_in_latency_window(servers.iter().collect(), false)
-                .unwrap();
+            server_selection::attempt_to_select_server(&read_pref, &topology_description, &servers)
+                .expect("selection should not fail")
+                .expect("a server should have been selected");
         *tallies.entry(selection.address.clone()).or_insert(0) += 1;
     }
 
-    for (address, expected_frequency) in test_file.expected_frequencies {
+    for (address, expected_frequency) in test_file.outcome.expected_frequencies {
         let actual_frequency =
-            tallies.get(&address).cloned().unwrap_or(0) as f64 / (TEST_ITERATIONS as f64);
+            tallies.get(&address).cloned().unwrap_or(0) as f64 / (test_file.iterations as f64);
 
         let epsilon = if expected_frequency != 1.0 && expected_frequency != 0.0 {
-            0.02
+            test_file.outcome.tolerance
         } else {
             f64::EPSILON
         };
